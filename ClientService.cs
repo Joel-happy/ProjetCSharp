86c915fb2dc317823cd0d31b78e81cb742e2f210
S@nji <107140032+Joel-happy@users.noreply.github.com>
2023-11-28 15:58:35 +0100

    initialisation

---
+++ ClientService.cs
@@ -0,0 +1,13 @@
+﻿using GestionIventaire;
+
+public class ClientService
+{
+    private List<Client> clients = new List<Client>();
+
+    public void AjouterClient(Client client)
+    {
+        // Logique pour ajouter un client
+    }
+
+    // Ajoutez les méthodes CRUD restantes
+}
