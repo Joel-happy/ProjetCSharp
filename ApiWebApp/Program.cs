59f5fd5a2ab474bcdc2d4ef365ee31f16f1827d4
MIHUTA Mihail <mihail.mihuta@ynov.com>
2023-12-12 10:28:37 +0100

    [ADD] select * from account database + display on localhost

--- ApiWebApp/Program.cs
+++ ApiWebApp/Program.cs
@@ -30,13 +30,13 @@ namespace ApiWebApp
 
         static async Task ProcessRequestAsync(TcpClient client)
         {
-            // Open a network stream for reading and writing data to the client.
+            // Open a network stream for reading and writing data to the client
             using (NetworkStream stream = client.GetStream())
 
-            // Create a StreamReader for reading data from the network stream.
+            // Create a StreamReader for reading data from the network stream
             using (StreamReader reader = new StreamReader(stream))
 
-            // Create a StreamWriter for writing data to the network stream.
+            // Create a StreamWriter for writing data to the network stream
             using (StreamWriter writer = new StreamWriter(stream))
             {
                 // Read the first line of the HTTP request
@@ -44,15 +44,51 @@ namespace ApiWebApp
 
                 // Display the received request in the console
                 Console.WriteLine($"Received request: {request}");
-
-                // Simple HTML response to the client (for testing purposes)
-                string response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Hello, World!</h1></body></html>";
+                
+                string result = ReadDataFromDatabase();
+                string response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body>" + result + "</body></html>";
 
                 // Write the HTTP response to the client
                 await writer.WriteAsync(response);
             }
-
+            
             client.Close();
+        }
+
+        static string ReadDataFromDatabase()
+        {
+            string databasePath = Path.Combine(GetProjectRoot(), "db", "db.sqlite");
+
+            using (SQLiteConnection connection = new SQLiteConnection($"Data Source ={databasePath}; Version = 3;"))
+            {
+                connection.Open();
+
+                using (SQLiteCommand command = new SQLiteCommand("SELECT * FROM account", connection))
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    StringBuilder result = new StringBuilder();
+
+                    while (reader.Read())
+                    {
+                        string username = reader.GetString(1);
+                        string email = reader.GetString(2);
+                        string password = reader.GetString(3);
+
+                        result.AppendLine($"Username : {username}, Email : {email}, Password : {password} \n");
+                    }
+
+                    connection.Close();
+                    return result.ToString();
+                }
+            }
+        }
+
+        // Returns project root path
+        static string GetProjectRoot()
+        {
+            // Logic to determine the project root directory
+            string executableDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            return Path.Combine(executableDirectory, "../../../../");
         }
     }
 }
